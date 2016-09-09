e667626a4a9a37e3a4e18e8298b5e64e5bca2921
Favre Anael <anael.favre@gmail.com>
2016-09-09 23:26:07 +0200

    Fixing missing dep and git clone

--- ubuntu-16.04/installer.sh
+++ ubuntu-16.04/installer.sh
@@ -5,12 +5,12 @@ apt update
 apt upgrade -y
 
 # Installing required packages
-apt install -y git php7.0-cli curl
+apt install -y git curl php7.0-cli php7.0-mbstring
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 
 # Installing 6admin
 cd /opt
-git clone git@github.com:6admin/6admin.git
+git clone https://github.com/6admin/6admin.git
 cd 6admin
 
 composer install
