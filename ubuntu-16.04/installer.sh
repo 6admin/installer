81d80ad107478e34ffe205ab43dfc9356f5dcee7
Favre Anael <anael.favre@gmail.com>
2016-09-13 21:16:42 +0200

    Update installer.sh

--- ubuntu-16.04/installer.sh
+++ ubuntu-16.04/installer.sh
@@ -8,13 +8,17 @@ apt upgrade -y
 apt install -y git curl php7.0-cli php7.0-mbstring php7.0-xml zip
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 
-# Installing 6admin
+# Cloning 6admin
 cd /opt
 git clone https://github.com/6admin/6admin.git
-cd 6admin
 
-composer update
+# Installing the six command
+cd 6admin
 chmod +x six
 ln -s /opt/6admin/six /usr/local/bin/six
+
+# Install core dependencies (update as we are on dev)
+cd core
+composer update
 
 export DEBIAN_FRONTEND=dialog
