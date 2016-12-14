d2de15be448b20be8f0d7fbe9af92bafaa76f025
Favre Anael <anael.favre@gmail.com>
2016-12-14 23:37:57 +0100

    Use apt-get and remove sudo

--- ubuntu-16.04/installer.sh
+++ ubuntu-16.04/installer.sh
@@ -1,12 +1,12 @@
 export DEBIAN_FRONTEND=noninteractive
 
 # Make sur that all the packages are up to date
-apt update
-apt upgrade -y
+apt-get update
+apt-get upgrade -y
 
 # Installing required packages
-apt install -y git curl php7.0-cli php7.0-mbstring php7.0-xml zip
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+apt-get install -y git curl php7.0-cli php7.0-mbstring php7.0-xml zip
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
 # Cloning 6admin
 cd /opt
